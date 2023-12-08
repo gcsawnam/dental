86b3fb1f938a32cef333f06554d2a812857cc464
gcsawnam <gcsawnam@gmail.com>
2023-12-08 16:39:38 +0545

    fixes

---
+++ patterns/faq.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Title: Faq
+ * Slug: dental/faq
+ */
+?>
+<!-- wp:group {"align":"full","style":{"color":{"background":"#d31d20"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-background" style="background-color:#d31d20"><!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"style":{"typography":{"fontSize":"48px","fontStyle":"normal","fontWeight":"600","lineHeight":"1"},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"background","fontFamily":"kanit"} -->
+<h2 class="wp-block-heading has-background-color has-text-color has-link-color has-kanit-font-family" style="font-size:48px;font-style:normal;font-weight:600;line-height:1">MURFREESBORO PRESSURE WASHING TN FAQs</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontSize":"15px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.9"}},"textColor":"background"} -->
+<p class="has-background-color has-text-color has-link-color" style="font-size:15px;font-style:normal;font-weight:400;line-height:1.9">At McCoy’s Pressure Washing, we understand that many of our customers may have questions about our services, pricing and process. To provide the best experience for our customers, we have compiled a list of frequently asked questions (FAQs) on our website. This FAQ page covers a range of topics, including what pressure washing is and how it works to what type of cleaning solutions we use and how to prepare for our services.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:getwid/accordion -->
+<div class="wp-block-getwid-accordion has-icon-left" data-active-element="none"><!-- wp:getwid/accordion-item -->
+<div class="wp-block-getwid-accordion__header-wrapper"><span class="wp-block-getwid-accordion__header"><a href="#"><span class="wp-block-getwid-accordion__header-title">WHAT SURFACES CAN YOU CLEAN WITH PRESSURE WASHING?</span><span class="wp-block-getwid-accordion__icon is-active"><i class="fas fa-plus"></i></span><span class="wp-block-getwid-accordion__icon is-passive"><i class="fas fa-minus"></i></span></a></span></div><div class="wp-block-getwid-accordion__content-wrapper"><div class="wp-block-getwid-accordion__content"><!-- wp:paragraph -->
+<p>We can effectively clean a wide range of surfaces, including concrete, brick, stucco, wood, vinyl, and more. Our advanced pressure washing equipment and eco-friendly cleaning solutions can safely and effectively remove dirt, grime, mold, and other tough stains from any surface.</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:getwid/accordion-item -->
+
+<!-- wp:getwid/accordion-item -->
+<div class="wp-block-getwid-accordion__header-wrapper"><span class="wp-block-getwid-accordion__header"><a href="#"><span class="wp-block-getwid-accordion__header-title">WHAT SURFACES CAN YOU CLEAN WITH PRESSURE WASHING?</span><span class="wp-block-getwid-accordion__icon is-active"><i class="fas fa-plus"></i></span><span class="wp-block-getwid-accordion__icon is-passive"><i class="fas fa-minus"></i></span></a></span></div><div class="wp-block-getwid-accordion__content-wrapper"><div class="wp-block-getwid-accordion__content"><!-- wp:paragraph -->
+<p>We can effectively clean a wide range of surfaces, including concrete, brick, stucco, wood, vinyl, and more. Our advanced pressure washing equipment and eco-friendly cleaning solutions can safely and effectively remove dirt, grime, mold, and other tough stains from any surface.</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:getwid/accordion-item -->
+
+<!-- wp:getwid/accordion-item -->
+<div class="wp-block-getwid-accordion__header-wrapper"><span class="wp-block-getwid-accordion__header"><a href="#"><span class="wp-block-getwid-accordion__header-title">WHAT SURFACES CAN YOU CLEAN WITH PRESSURE WASHING?</span><span class="wp-block-getwid-accordion__icon is-active"><i class="fas fa-plus"></i></span><span class="wp-block-getwid-accordion__icon is-passive"><i class="fas fa-minus"></i></span></a></span></div><div class="wp-block-getwid-accordion__content-wrapper"><div class="wp-block-getwid-accordion__content"><!-- wp:paragraph -->
+<p>We can effectively clean a wide range of surfaces, including concrete, brick, stucco, wood, vinyl, and more. Our advanced pressure washing equipment and eco-friendly cleaning solutions can safely and effectively remove dirt, grime, mold, and other tough stains from any surface.</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:getwid/accordion-item -->
+
+<!-- wp:getwid/accordion-item -->
+<div class="wp-block-getwid-accordion__header-wrapper"><span class="wp-block-getwid-accordion__header"><a href="#"><span class="wp-block-getwid-accordion__header-title">WHAT SURFACES CAN YOU CLEAN WITH PRESSURE WASHING?</span><span class="wp-block-getwid-accordion__icon is-active"><i class="fas fa-plus"></i></span><span class="wp-block-getwid-accordion__icon is-passive"><i class="fas fa-minus"></i></span></a></span></div><div class="wp-block-getwid-accordion__content-wrapper"><div class="wp-block-getwid-accordion__content"><!-- wp:paragraph -->
+<p>We can effectively clean a wide range of surfaces, including concrete, brick, stucco, wood, vinyl, and more. Our advanced pressure washing equipment and eco-friendly cleaning solutions can safely and effectively remove dirt, grime, mold, and other tough stains from any surface.</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:getwid/accordion-item -->
+
+<!-- wp:getwid/accordion-item -->
+<div class="wp-block-getwid-accordion__header-wrapper"><span class="wp-block-getwid-accordion__header"><a href="#"><span class="wp-block-getwid-accordion__header-title">WHAT SURFACES CAN YOU CLEAN WITH PRESSURE WASHING?</span><span class="wp-block-getwid-accordion__icon is-active"><i class="fas fa-plus"></i></span><span class="wp-block-getwid-accordion__icon is-passive"><i class="fas fa-minus"></i></span></a></span></div><div class="wp-block-getwid-accordion__content-wrapper"><div class="wp-block-getwid-accordion__content"><!-- wp:paragraph -->
+<p>We can effectively clean a wide range of surfaces, including concrete, brick, stucco, wood, vinyl, and more. Our advanced pressure washing equipment and eco-friendly cleaning solutions can safely and effectively remove dirt, grime, mold, and other tough stains from any surface.</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:getwid/accordion-item -->
+
+<!-- wp:getwid/accordion-item -->
+<div class="wp-block-getwid-accordion__header-wrapper"><span class="wp-block-getwid-accordion__header"><a href="#"><span class="wp-block-getwid-accordion__header-title">WHAT SURFACES CAN YOU CLEAN WITH PRESSURE WASHING?</span><span class="wp-block-getwid-accordion__icon is-active"><i class="fas fa-plus"></i></span><span class="wp-block-getwid-accordion__icon is-passive"><i class="fas fa-minus"></i></span></a></span></div><div class="wp-block-getwid-accordion__content-wrapper"><div class="wp-block-getwid-accordion__content"><!-- wp:paragraph {"placeholder":"Write text…"} -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam rhoncus neque eget eros aliquet, sit amet dignissim nisl venenatis.</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:getwid/accordion-item --></div>
+<!-- /wp:getwid/accordion -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
---
+++ patterns/single-post.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Title:single-post
+ * Slug: dental/single-post
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|black"}}}},"textColor":"black"} -->
+<p class="has-text-align-center has-black-color has-text-color has-link-color" style="font-size:16px;font-style:normal;font-weight:600">Creating Brighter Smiles</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"48px","fontStyle":"normal","fontWeight":"600"},"spacing":{"margin":{"right":"0","left":"0","top":"0","bottom":"0"},"padding":{"top":"0","bottom":"0"}}},"fontFamily":"kanit"} -->
+<h2 class="wp-block-heading has-text-align-center has-kanit-font-family" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;padding-top:0;padding-bottom:0;font-size:48px;font-style:normal;font-weight:600">The Swish Smile Blog</h2>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|70"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:query {"queryId":18,"query":{"perPage":6,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","inherit":false}} -->
+<div class="wp-block-query"><!-- wp:spacer {"height":"10px","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<div style="margin-top:0;margin-bottom:0;height:10px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:post-template {"textColor":"paragraph","layout":{"type":"grid","columnCount":3}} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"layout":{"inherit":false}} -->
+<div class="wp-block-group"><!-- wp:post-featured-image {"height":"300px","style":{"border":{"radius":{"topRight":"129px"}}}} /-->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}},"border":{"width":"1px"}},"borderColor":"hover","layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group has-border-color has-hover-border-color" style="border-width:1px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"style":{"typography":{"fontSize":"0.75rem","fontStyle":"normal","fontWeight":"600"}},"className":"post-border-bottom"} -->
+<p class="post-border-bottom" style="font-size:0.75rem;font-style:normal;font-weight:600">ORAL HEALTH</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:post-title {"isLink":true,"style":{"typography":{"fontSize":"1.5rem","fontStyle":"normal","fontWeight":"600","lineHeight":"1.4"},"elements":{"link":{"color":{"text":"#1a3743"}}},"color":{"text":"#1a3743"}}} /-->
+
+<!-- wp:post-content {"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"400"}},"textColor":"paragraph"} /-->
+
+<!-- wp:read-more {"content":"Read the Post","style":{"elements":{"link":{"color":{"text":"var:preset|color|button-hover"}}}},"textColor":"button-hover","className":"read-the-post"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+<!-- /wp:post-template --></div>
+<!-- /wp:query --></div>
+<!-- /wp:group -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","style":{"border":{"radius":{"bottomLeft":"26px","topRight":"26px"}}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-text-align-center wp-element-button" style="border-top-right-radius:26px;border-bottom-left-radius:26px">Explore All Blog Post</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
