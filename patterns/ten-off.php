fa4f5495503ba5aec65b447bc1c4c1c073fef40c
gcsawnam <gcsawnam@gmail.com>
2023-12-09 20:00:28 +0545

    fixes

---
+++ patterns/ten-off.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Title: Ten-Off
+ * Slug: dental/ten-off
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:cover {"url":"http://localhost/project/wp-content/uploads/2023/12/94tgj4t.png","id":361,"dimRatio":80,"customOverlayColor":"#4268d2","style":{"border":{"radius":{"topRight":"150px","bottomLeft":"150px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover" style="border-top-right-radius:150px;border-bottom-left-radius:150px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-80 has-background-dim" style="background-color:#4268d2"></span><img class="wp-block-cover__image-background wp-image-361" alt="" src="http://localhost/project/wp-content/uploads/2023/12/94tgj4t.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:columns {"style":{"spacing":{"margin":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"},"padding":{"right":"0","left":"0"}}}} -->
+<div class="wp-block-columns" style="margin-top:var(--wp--preset--spacing--80);margin-bottom:var(--wp--preset--spacing--80);padding-right:0;padding-left:0"><!-- wp:column {"width":"66.66%","style":{"spacing":{"blockGap":"var:preset|spacing|40","padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-column" style="padding-right:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40);flex-basis:66.66%"><!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"76px","fontStyle":"normal","fontWeight":"700","lineHeight":"1.2"}}} -->
+<h2 class="wp-block-heading has-text-align-center" style="font-size:76px;font-style:normal;font-weight:700;line-height:1.2">Get&nbsp;10%-Off&nbsp;Your</h2>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"76px","fontStyle":"normal","fontWeight":"700","lineHeight":"1.2"}}} -->
+<h2 class="wp-block-heading has-text-align-center" style="font-size:76px;font-style:normal;font-weight:700;line-height:1.2">First Visit</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"18px"}}} -->
+<p class="has-text-align-center" style="font-size:18px">If you are in need of high-quality, professional and friendly dental care, look no further than our clinic.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"border":{"radius":"50px"},"spacing":{"padding":{"left":"var:preset|spacing|70","right":"var:preset|spacing|70","top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" style="border-radius:50px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--70)">Book Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:image {"id":352,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost/project/wp-content/uploads/2023/12/4tihg4tg.png" alt="" class="wp-image-352"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:spacer -->
+<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
