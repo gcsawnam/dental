cf50eb9c2b8a756f440cba129d070ade673c716b
rameshdevkota23 <devkotaramesh79@gmail.com>
2023-12-09 22:31:59 +0545

    fixes

---
+++ patterns/about-us.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Title: About Us
+ * Slug: dental/about-us
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:columns {"style":{"spacing":{"blockGap":{"left":"var:preset|spacing|70"}}}} -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":10826,"aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost/corporate/wp-content/uploads/2023/12/home-222-2-1.jpg" alt="" class="wp-image-10826" style="aspect-ratio:1;object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"color":{"text":"#0a6076"},"elements":{"link":{"color":{"text":"#0a6076"}}},"typography":{"fontSize":"22px","fontStyle":"normal","fontWeight":"300"}}} -->
+<p class="has-text-color has-link-color" style="color:#0a6076;font-size:22px;font-style:normal;font-weight:300">About Us</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"300","lineHeight":"1.3"},"color":{"text":"#292834"},"elements":{"link":{"color":{"text":"#292834"}}}}} -->
+<h2 class="wp-block-heading has-text-color has-link-color" style="color:#292834;font-style:normal;font-weight:300;line-height:1.3">Catering to all of your dental needs and desires</h2>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"8px"} -->
+<div style="height:8px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.4"}}} -->
+<p style="line-height:1.4">Our Dental Clinic is dedicated to providing the most up to date general, orthodontic and family dentistry.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Our Clinic has grown to provide a world class facility for the treatment of tooth loss, dental cosmetics and advanced restorative dentistry.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"16px"} -->
+<div style="height:16px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Learn More</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"color":{"background":"#09687e"},"spacing":{"padding":{"right":"0","left":"0","top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group has-background" style="background-color:#09687e;padding-top:var(--wp--preset--spacing--80);padding-right:0;padding-bottom:var(--wp--preset--spacing--80);padding-left:0"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"textColor":"background"} -->
+<h2 class="wp-block-heading has-text-align-center has-background-color has-text-color has-link-color" style="font-style:normal;font-weight:300">59</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"textColor":"background"} -->
+<p class="has-text-align-center has-background-color has-text-color has-link-color" style="font-style:normal;font-weight:300">Great Dentists</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"textColor":"background"} -->
+<h2 class="wp-block-heading has-text-align-center has-background-color has-text-color has-link-color" style="font-style:normal;font-weight:300">63</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"textColor":"background"} -->
+<p class="has-text-align-center has-background-color has-text-color has-link-color" style="font-style:normal;font-weight:300">Dentists Specilities</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"textColor":"background"} -->
+<h2 class="wp-block-heading has-text-align-center has-background-color has-text-color has-link-color" style="font-style:normal;font-weight:300">18</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"textColor":"background"} -->
+<p class="has-text-align-center has-background-color has-text-color has-link-color" style="font-style:normal;font-weight:300">Dental service</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"textColor":"background"} -->
+<h2 class="wp-block-heading has-text-align-center has-background-color has-text-color has-link-color" style="font-style:normal;font-weight:300">18</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"textColor":"background"} -->
+<p class="has-text-align-center has-background-color has-text-color has-link-color" style="font-style:normal;font-weight:300">National Award</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"textColor":"background"} -->
+<h2 class="wp-block-heading has-text-align-center has-background-color has-text-color has-link-color" style="font-style:normal;font-weight:300">14</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontStyle":"normal","fontWeight":"300"}},"textColor":"background"} -->
+<p class="has-text-align-center has-background-color has-text-color has-link-color" style="font-style:normal;font-weight:300">Years of Experiences</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
