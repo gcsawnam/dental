df646a4b2501d4d24bfe89b567b16be1d32f32a1
gcsawnam <gcsawnam@gmail.com>
2023-12-09 10:30:32 +0545

    fixes

--- patterns/single-post.php
+++ patterns/single-post.php
@@ -29,7 +29,9 @@
 
 <!-- wp:post-template {"textColor":"paragraph","layout":{"type":"grid","columnCount":3}} -->
 <!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"layout":{"inherit":false}} -->
-<div class="wp-block-group"><!-- wp:post-featured-image {"height":"300px","style":{"border":{"radius":{"topRight":"129px"}}}} /-->
+<div class="wp-block-group"><!-- wp:group {"className":"single-post-over-flow-hidden","layout":{"type":"constrained"}} -->
+<div class="wp-block-group single-post-over-flow-hidden"><!-- wp:post-featured-image {"height":"300px","style":{"border":{"radius":{"topRight":"129px"}}},"className":"single-post-img"} /--></div>
+<!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}},"border":{"width":"1px"}},"borderColor":"hover","layout":{"type":"constrained","justifyContent":"left"}} -->
 <div class="wp-block-group has-border-color has-hover-border-color" style="border-width:1px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"style":{"typography":{"fontSize":"0.75rem","fontStyle":"normal","fontWeight":"600"}},"className":"post-border-bottom"} -->
