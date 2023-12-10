e84c8c01258360ade127b4f4f3ee1a6914503bf3
rameshdevkota23 <devkotaramesh79@gmail.com>
2023-12-10 11:27:14 +0545

    fixes

---
+++ patterns/dental-care.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Title: Dental Care
+ * Slug: dental/dental-care
+ */
+?>
+<!-- wp:group {"style":{"color":{"background":"#f0f5f5"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-background" style="background-color:#f0f5f5"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:heading {"style":{"typography":{"fontSize":"39px","fontStyle":"normal","fontWeight":"300"},"color":{"text":"#343434"},"elements":{"link":{"color":{"text":"#343434"}}}}} -->
+<h2 class="wp-block-heading has-text-color has-link-color" style="color:#343434;font-size:39px;font-style:normal;font-weight:300">Dental Care is not</h2>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"style":{"typography":{"fontSize":"39px","fontStyle":"normal","fontWeight":"700"},"color":{"text":"#00bcd5"},"elements":{"link":{"color":{"text":"#00bcd5"}}}}} -->
+<h2 class="wp-block-heading has-text-color has-link-color" style="color:#00bcd5;font-size:39px;font-style:normal;font-weight:700"><strong>Expensive...Neglet is</strong></h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"300"},"color":{"text":"#959595"},"elements":{"link":{"color":{"text":"#959595"}}}}} -->
+<p class="has-text-align-center has-text-color has-link-color" style="color:#959595;font-style:normal;font-weight:300">Healthy Lifestyle - Prerequisite for dental hygiene</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"36px"} -->
+<div style="height:36px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}}},"backgroundColor":"background","layout":{"type":"default"}} -->
+<div class="wp-block-group has-background-background-color has-background" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--60)"><!-- wp:image {"align":"center","id":11051,"width":"50px","height":"50px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://localhost/corporate/wp-content/uploads/2023/12/tooth.png" alt="" class="wp-image-11051" style="object-fit:cover;width:50px;height:50px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"600","fontSize":"15px"}}} -->
+<h4 class="wp-block-heading has-text-align-center" style="font-size:15px;font-style:normal;font-weight:600">Awesome Service</h4>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}}},"backgroundColor":"background","layout":{"type":"default"}} -->
+<div class="wp-block-group has-background-background-color has-background" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--60)"><!-- wp:image {"align":"center","id":11051,"width":"50px","height":"50px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://localhost/corporate/wp-content/uploads/2023/12/tooth.png" alt="" class="wp-image-11051" style="object-fit:cover;width:50px;height:50px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"600","fontSize":"15px"}}} -->
+<h4 class="wp-block-heading has-text-align-center" style="font-size:15px;font-style:normal;font-weight:600">Awesome Service</h4>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}}},"backgroundColor":"background","layout":{"type":"default"}} -->
+<div class="wp-block-group has-background-background-color has-background" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--60)"><!-- wp:image {"align":"center","id":11051,"width":"50px","height":"50px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://localhost/corporate/wp-content/uploads/2023/12/tooth.png" alt="" class="wp-image-11051" style="object-fit:cover;width:50px;height:50px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"600","fontSize":"15px"}}} -->
+<h4 class="wp-block-heading has-text-align-center" style="font-size:15px;font-style:normal;font-weight:600">Awesome Service</h4>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}}},"backgroundColor":"background","layout":{"type":"default"}} -->
+<div class="wp-block-group has-background-background-color has-background" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--60)"><!-- wp:image {"align":"center","id":11051,"width":"50px","height":"50px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://localhost/corporate/wp-content/uploads/2023/12/tooth.png" alt="" class="wp-image-11051" style="object-fit:cover;width:50px;height:50px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"600","fontSize":"15px"}}} -->
+<h4 class="wp-block-heading has-text-align-center" style="font-size:15px;font-style:normal;font-weight:600">Awesome Service</h4>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}}},"backgroundColor":"background","layout":{"type":"default"}} -->
+<div class="wp-block-group has-background-background-color has-background" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--60)"><!-- wp:image {"align":"center","id":11051,"width":"50px","height":"50px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://localhost/corporate/wp-content/uploads/2023/12/tooth.png" alt="" class="wp-image-11051" style="object-fit:cover;width:50px;height:50px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"600","fontSize":"15px"}}} -->
+<h4 class="wp-block-heading has-text-align-center" style="font-size:15px;font-style:normal;font-weight:600">Awesome Service</h4>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}}},"backgroundColor":"background","layout":{"type":"default"}} -->
+<div class="wp-block-group has-background-background-color has-background" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--60)"><!-- wp:image {"align":"center","id":11051,"width":"50px","height":"50px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://localhost/corporate/wp-content/uploads/2023/12/tooth.png" alt="" class="wp-image-11051" style="object-fit:cover;width:50px;height:50px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"600","fontSize":"15px"}}} -->
+<h4 class="wp-block-heading has-text-align-center" style="font-size:15px;font-style:normal;font-weight:600">Awesome Service</h4>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}}},"backgroundColor":"background","layout":{"type":"default"}} -->
+<div class="wp-block-group has-background-background-color has-background" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--60)"><!-- wp:image {"align":"center","id":11051,"width":"50px","height":"50px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://localhost/corporate/wp-content/uploads/2023/12/tooth.png" alt="" class="wp-image-11051" style="object-fit:cover;width:50px;height:50px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"600","fontSize":"15px"}}} -->
+<h4 class="wp-block-heading has-text-align-center" style="font-size:15px;font-style:normal;font-weight:600">Awesome Service</h4>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|60","right":"var:preset|spacing|60"}}},"backgroundColor":"background","layout":{"type":"default"}} -->
+<div class="wp-block-group has-background-background-color has-background" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--60)"><!-- wp:image {"align":"center","id":11051,"width":"50px","height":"50px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://localhost/corporate/wp-content/uploads/2023/12/tooth.png" alt="" class="wp-image-11051" style="object-fit:cover;width:50px;height:50px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":4,"style":{"typography":{"fontStyle":"normal","fontWeight":"600","fontSize":"15px"}}} -->
+<h4 class="wp-block-heading has-text-align-center" style="font-size:15px;font-style:normal;font-weight:600">Awesome Service</h4>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"15px"} -->
+<div style="height:15px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
